2c1d3ba344e7191a7e03eb5dccc3785afd06dadf
RubaCodes <rubacodes@gmail.com>
2022-09-15 16:00:43 +0200

    cibo di meta classifica , troncato

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿//cibi-preferiti c#
 
 //inizializzare l'array dei cibi preferiti min 5 / max 10
-string[] favoriteFoods = { "Pizza", "Pasta", "Gelato", "Sushi", "Kebab","Hamburger", "Fiorentina", "Cioccolato" };
+string[] favoriteFoods = { "Pizza", "Pasta", "Gelato", "Sushi", "Kebab","Hamburger", "Fiorentina", "Cioccolato", "Meringa" };
 
 //lunghezza della classifica
 Console.WriteLine("la classifica e' lunga : " + favoriteFoods.Length + " elementi");
@@ -12,4 +12,7 @@ for (int i = 0; i < favoriteFoods.Length; i++) {
 //cibo preferito
 Console.WriteLine("Il mio cibo preferito e': "+ favoriteFoods[0]);
 //cibo preferito (ma non troppo) ultima posizione della classifica
-Console.WriteLine("Il mio cibo preferito e': "+ favoriteFoods[favoriteFoods.Length -1]);
+Console.WriteLine("Il mio cibo meno e': "+ favoriteFoods[favoriteFoods.Length -1]);
+//cibo di meta'
+int midIndex = Convert.ToInt32(Math.Floor(((double)favoriteFoods.Length - 1) / 2));
+Console.WriteLine("Il mio cibo di meta' classifica e':" + favoriteFoods[midIndex]);

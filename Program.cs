6e98f9357b03ef6dccab907e54c76a20ff0ab515
RubaCodes <rubacodes@gmail.com>
2022-09-15 15:25:26 +0200

    stampa lunghezza classifica

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿//cibi-preferiti c#
 
 //inizializzare l'array dei cibi preferiti min 5 / max 10
-
 string[] favoriteFoods = { "Pizza", "Pasta", "Gelato", "Sushi", "Kebab","Hamburger", "Fiorentina", "Cioccolato" };
+
+//lunghezza della classifica
+Console.WriteLine("la classifica e' lunga :" + favoriteFoods.Length + " elementi");

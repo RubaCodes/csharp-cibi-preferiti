574eebe6210da1bc82e53cad8ba64e95fd94010d
RubaCodes <rubacodes@gmail.com>
2022-09-15 15:30:09 +0200

    Inserito cibo preferito e meno preferito

--- Program.cs
+++ Program.cs
@@ -4,4 +4,12 @@
 string[] favoriteFoods = { "Pizza", "Pasta", "Gelato", "Sushi", "Kebab","Hamburger", "Fiorentina", "Cioccolato" };
 
 //lunghezza della classifica
-Console.WriteLine("la classifica e' lunga :" + favoriteFoods.Length + " elementi");
+Console.WriteLine("la classifica e' lunga : " + favoriteFoods.Length + " elementi");
+Console.WriteLine("La classifica consiste in:");
+for (int i = 0; i < favoriteFoods.Length; i++) {
+    Console.WriteLine(favoriteFoods[i]);
+}
+//cibo preferito
+Console.WriteLine("Il mio cibo preferito e': "+ favoriteFoods[0]);
+//cibo preferito (ma non troppo) ultima posizione della classifica
+Console.WriteLine("Il mio cibo preferito e': "+ favoriteFoods[favoriteFoods.Length -1]);
